7b71c1c0fd6c07704e3dd516232486db8eacf2b2
Jamie Dawson <jadawson@e1z3r2p17.42.us.org>
2018-08-27 22:35:39 -0700

    updated code. Now half the size it use to be

--- level1/rot_13/rot_13.c
+++ level1/rot_13/rot_13.c
@@ -6,42 +6,39 @@
 /*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/01 15:11:56 by jadawson          #+#    #+#             */
-/*   Updated: 2018/08/01 15:14:37 by jadawson         ###   ########.fr       */
+/*   Updated: 2018/08/27 22:35:12 by jadawson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void rot_13(char *str)
+void	rot_13(char *str)
 {
 	int i;
 
 	i = 0;
 	while(str[i] != '\0')
 	{
-		if (str[i] >= 'A' && str[i] <= 'M')
+		if (str[i] >= 'A' && str[i] <= 'M' || str[i] >= 'a' && str[i] <= 'm')
 			ft_putchar(str[i] + 13);
-		else if (str[i] >= 'N' && str[i] <= 'Z')
-			ft_putchar(str[i] - 13);
-		else if (str[i] >= 'a' && str[i] <= 'm')
-			ft_putchar(str[i] + 13);
-		else if (str[i] >= 'n' && str[i] <= 'z')
+		else if (str[i] >= 'N' && str[i] <= 'Z' || str[i] >= 'n' && str[i] <= 'z')
 			ft_putchar(str[i] - 13);
 		else
 			ft_putchar(str[i]);
 		i++;
-	}
+     }
 }
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-	if(argc == 2)
-		rot_13(argv[1]);
+	if (argc == 2)
+     	rot_13(argv[1]);
 	ft_putchar('\n');
 	return (0);
 }

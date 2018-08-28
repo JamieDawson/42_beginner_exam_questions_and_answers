9880cc01d6cb0fd87468a896184728b1c0811f05
Jamie Dawson <jadawson@e1z3r2p17.42.us.org>
2018-08-27 22:25:35 -0700

    updated code. Much smaller than before

--- level1/repeat_alpha/repeat_alpha.c
+++ level1/repeat_alpha/repeat_alpha.c
@@ -6,57 +6,49 @@
 /*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/01 15:01:51 by jadawson          #+#    #+#             */
-/*   Updated: 2018/08/01 15:04:57 by jadawson         ###   ########.fr       */
+/*   Updated: 2018/08/27 22:25:22 by jadawson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
-void    ft_putchar(char c)
+void	ft_putchar (char c)
 {
-	write(1, &c, 1);
+	write (1, &c, 1);
 }
 
-char    ft_repeat_alpha(char *str)
+void	repeat_alpha (char *str)
 {
 	int i;
 	int j;
+
 	i = 0;
 	j = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 'A' && str[i] <= 'Z')  //finds uppercase letters.
+		if (str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'a' && str[i] <= 'z')
 		{
-			j = str[i] - 65; //65 is an ascii value of 'A'
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				j = str[i] - 65;
+			else if (str[i] >= 'a' && str[i] <= 'z')
+				j = str[i] - 97;
 			while (j >= 0)
 			{
-				ft_putchar(str[i]);
+				ft_putchar (str[i]);
 				j--;
 			}
 		}
-		else if (str[i] >= 'a' && str[i] <= 'z')   //finds lowercase letters
-		{
-			j = str[i] - 97; //97 is the ascii value of 'a'
-			while (j >= 0)
-			{
-				ft_putchar(str[i]);
-				j--;
-			}
-		}
-		else   //finds non letter characters
-		{
+		if (!(str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
 			ft_putchar(str[i]);
-		}
-		i++;   //makes sure i is increasing inside while loop.
+		i++;
 	}
-	ft_putchar('\n'); //creates newline when loop is done.
 }
 
-int        main(int argc, char **argv)
+int main (int argc, char **argv)
 {
 	if (argc == 2)
-		ft_repeat_alpha(argv[1]);
-	else
-		ft_putchar('\n');
+		repeat_alpha (argv[1]);
+	ft_putchar ('\n');
 	return (0);
 }

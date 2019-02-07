6be7bdd2131767995dfd4bd4f9af9870725f85e7
Jamie Dawson <jadawson@e1z3r3p23.42.us.org>
2019-02-07 14:38:29 -0800

    find total arguements passed to function

---
+++ level3/paramsum/paramsum.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   paramsum.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/07 14:37:33 by jadawson          #+#    #+#             */
+/*   Updated: 2019/02/07 14:37:57 by jadawson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void    ft_putnum(int num)
+{
+	if (num / 10)
+	{
+		ft_putnum (num / 10);
+		ft_putnum (num % 10);
+	}
+	else
+		ft_putchar(num + '0');
+}
+
+int        main(int argc, char **argv)
+{
+	(void)argv;
+	if (argc == 1)
+	{
+		write(1, "0\n", 2);
+		return (0);
+	}
+	if (argc > 1)
+	{
+		ft_putnum(argc - 1);
+		ft_putchar('\n');
+		return (0);
+	}
+	return (0);
+}
+

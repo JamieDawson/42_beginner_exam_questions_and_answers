e49ea686858508d0593fe84f15e61a2fb67d0b35
Jamie Dawson <jadawson@e1z3r3p23.42.us.org>
2019-02-07 14:42:29 -0800

    updated with notes inside .c file

--- level3/paramsum/paramsum.c
+++ level3/paramsum/paramsum.c
@@ -6,7 +6,7 @@
 /*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/07 14:37:33 by jadawson          #+#    #+#             */
-/*   Updated: 2019/02/07 14:37:57 by jadawson         ###   ########.fr       */
+/*   Updated: 2019/02/07 14:42:17 by jadawson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,3 +45,15 @@ int        main(int argc, char **argv)
 	return (0);
 }
 
+
+/*
+HOW IT WORKS:
+
+Setting argv to void since we are not using it. If you type "gcc -Wall -Wextra -Werror paramsum.c", you'll see the error pop up telling you you're not using the variable.
+
+pass in arguemnts example:   a./out test1 test2 test3
+argc counts 1 2 3 4 5
+argv counts 0 1 2 3 4
+
+By subtracting 1 from argc, you get the number of arguements.
+*/

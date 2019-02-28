ab8d0c10d485ff6c6661d7892b0771e2746d886b
Jamie Dawson <jadawson@e1z2r5p11.42.us.org>
2019-02-27 17:57:22 -0800

    made code smaller

--- level1/repeat_alpha/repeat_alpha.c
+++ level1/repeat_alpha/repeat_alpha.c
@@ -6,49 +6,39 @@
 /*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/01 15:01:51 by jadawson          #+#    #+#             */
-/*   Updated: 2018/08/27 22:25:22 by jadawson         ###   ########.fr       */
+/*   Updated: 2019/02/27 17:57:13 by jadawson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
-void	ft_putchar (char c)
+void	ft_putchar(char c)
 {
-	write (1, &c, 1);
+	write(1, &c, 1);
 }
 
-void	repeat_alpha (char *str)
+void	repeat_alpha(char *s)
 {
-	int i;
+	int i = -1;
 	int j;
-
-	i = 0;
-	j = 0;
-	while (str[i] != '\0')
+	while (s[++i])
 	{
-		if (str[i] >= 'A' && str[i] <= 'Z' || str[i] >= 'a' && str[i] <= 'z')
-		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-				j = str[i] - 65;
-			else if (str[i] >= 'a' && str[i] <= 'z')
-				j = str[i] - 97;
-			while (j >= 0)
-			{
-				ft_putchar (str[i]);
-				j--;
-			}
-		}
-		if (!(str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
-			ft_putchar(str[i]);
-		i++;
+		j = -1;
+		if (s[i] >= 'a' && s[i] <= 'z')
+			j = s[i] - 'a';
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			j = s[i] - 'A';
+		while (j-- >= 0)
+			ft_putchar(s[i]);
+		if (!(s[i] >= 'a' && s[i] <= 'z') && (!(s[i] >= 'A' && s[i] <= 'Z')))
+			ft_putchar(s[i]);
 	}
 }
 
-int main (int argc, char **argv)
+int		main(int argc, char **argv)
 {
 	if (argc == 2)
-		repeat_alpha (argv[1]);
-	ft_putchar ('\n');
+		repeat_alpha(argv[1]);
+	ft_putchar('\n');
 	return (0);
 }

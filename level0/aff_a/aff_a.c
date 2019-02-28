e4371e5470edbe68d6b3bf59a9f05d1be9059fc5
Jamie Dawson <jadawson@e1z2r5p11.42.us.org>
2019-02-27 17:34:33 -0800

    made code smaller

--- level0/aff_a/aff_a.c
+++ level0/aff_a/aff_a.c
@@ -1,29 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aff_a.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jadawson <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/27 17:34:18 by jadawson          #+#    #+#             */
+/*   Updated: 2019/02/27 17:34:25 by jadawson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-char    aff_a(char *str1)
+int		aff_a(char *s)
 {
-	int i;
-	i = 0;
-	while(str1[i])
-	{
-		if(str1[i] == 'a')
-		{
-			write(1, "a", 1);  
-			return 0;
-		}
-		i++;
-	}
-	return 0;
+	int i = -1;
+
+	while (s[++i])
+		if (s[i] == 'a')
+			return (1);
+	return (0);
 }
 
-int     main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
-		aff_a(argv[1]);
+		aff_a(argv[1]) == 1 ? write(1, "a\n", 2) : write(1, "\n", 1);
+		return (0);
 	}
-	else
-		write(1, "a", 1);
-	write(1, "\n", 1);
+	write(1, "a\n", 2);
 	return (0);
 }
